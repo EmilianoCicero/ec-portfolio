8ace0655a379c59b2c311e3a2476d04c46c58173
Emi <emiliano@Emis-Mac-mini.local>
2025-04-25 08:45:31 +0200

    added text size tokens

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<link rel="stylesheet" href="./tokens/ec-tokens.css"></link>
+import "./tokens/ec-tokens.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
